9e9b08b203c08a7d4e39dc44f79abae394744f45
Radek blufor Slavicinsky <radek.slavicinsky@gmail.com>
2025-12-12 18:38:38 +0100

    cleanup

--- update_gzw.sh
+++ update_gzw.sh
@@ -6,34 +6,34 @@ set -euo pipefail
 
 # find steamcmd
 _detect_steamcmd() {
-	for cmd in "${HOME}/steamcmd/steamcmd.sh" $(command -v steamcmd.sh) $(command -v steamcmd); do
-		if [ -x "$cmd" ]
-		then
-			echo "$cmd"
-			break
-		fi
-	done
+  for cmd in "${HOME}/steamcmd/steamcmd.sh" $(command -v steamcmd.sh) $(command -v steamcmd); do
+    if [ -x "$cmd" ]
+    then
+      echo "$cmd"
+      break
+    fi
+  done
 }
 
 # steamcmd install wrapper
 _install_steamcmd() {
-	mkdir -p ${HOME}/steamcmd
-	if command -v curl &> /dev/null
-	then
-		fetch='curl -sSLo-'
-	else
-		fetch='wget -qO-'
-	fi
-	$fetch 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' 2>/dev/null \
-		| tar -C ${HOME}/steamcmd -xzf -
-	echo "steamcmd installed for you in ${HOME}/steamcmd - but you should be a nice linux user and install one properly using a package manager!" >&2
+  mkdir -p ${HOME}/steamcmd
+  if command -v curl &> /dev/null
+  then
+    fetch='curl -sSLo-'
+  else
+    fetch='wget -qO-'
+  fi
+  $fetch 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' 2>/dev/null \
+    | tar -C ${HOME}/steamcmd -xzf -
+  echo "steamcmd installed for you in ${HOME}/steamcmd - but you should be a nice linux user and install one properly using a package manager!" >&2
 }
 
 # make sure we have steamcmd
 STEAMCMD=$(_detect_steamcmd)
 if [ -z "${STEAMCMD}" ]; then
-	_install_steamcmd
-	STEAMCMD=${HOME}/steamcmd/steamcmd.sh
+  _install_steamcmd
+  STEAMCMD=${HOME}/steamcmd/steamcmd.sh
 fi
 
 # find steam dir
@@ -44,68 +44,68 @@ REVERT=false
 # read command line options
 while getopts "d:u:hr" opt
 do
-	case ${opt} in
-		u)
-			STEAM_USERNAME=${OPTARG}
-			;;
-		d)
-			STEAM_DIR=${OPTARG}
-			;;
-		r)
-			REVERT=true
-			;;
-		h)
-			echo "Usage: $0 [-d <steam_dir>] [-u <steam_username>] [-r]" >&2
-			exit 0
-			;;
-		\?)
-			echo "Usage: $0 [-d <steam_dir>] [-u <steam_username>] [-r]" >&2
-			echo "Invalid option: -${OPTARG}" >&2
-			exit 1
-			;;
-	esac
+  case ${opt} in
+    u)
+      STEAM_USERNAME=${OPTARG}
+      ;;
+    d)
+      STEAM_DIR=${OPTARG}
+      ;;
+    r)
+      REVERT=true
+      ;;
+    h)
+      echo "Usage: $0 [-d <steam_dir>] [-u <steam_username>] [-r]" >&2
+      exit 0
+      ;;
+    \?)
+      echo "Usage: $0 [-d <steam_dir>] [-u <steam_username>] [-r]" >&2
+      echo "Invalid option: -${OPTARG}" >&2
+      exit 1
+      ;;
+  esac
 done
 
 GAME_DIR="steamapps/common/Gray Zone Warfare"
 
 if [ -z "${STEAM_DIR}/steamapps" ]
 then
-	echo "Steam directory not found. Change Steam directory with -d option." >&2
-	exit 1
+  echo "Steam directory not found. Change Steam directory with -d option." >&2
+  exit 1
 fi
 
 if [ ! -d "${STEAM_DIR}/${GAME_DIR}" ]
 then
-	echo "Game directory ${STEAM_DIR}/${GAME_DIR} not found. Change Steam directory with -d option." >&2
-	exit 1
+  echo "Game directory '${STEAM_DIR}/${GAME_DIR}' not found. Change Steam directory with -d option." >&2
+  exit 1
 fi
 
 if [ ! -d "${GZW_DIR}" ]; then
-        echo "Game directory ${GZW_DIR} not found. Change Steam directory with -d option." >&2
+        echo "Game directory '${GZW_DIR}' not found. Change Steam directory with -d option." >&2
         exit 1
 fi
 
 declare -a FILES=(
-	${STEAM_DIR}/${GAME_DIR}/GZW/Content/SKALLA/PrebuildWorldData/World/cache/0xaf497c273f87b6e4_0x7a22fc105639587d.dat
-	${STEAM_DIR}/${GAME_DIR}/GZW/Content/SKALLA/PrebuildWorldData/World/cache/0xb9af63cee2e43b6c_0x3cb3b3354fb31606.dat
+  ${STEAM_DIR}/${GAME_DIR}/GZW/Content/SKALLA/PrebuildWorldData/World/cache/0xaf497c273f87b6e4_0x7a22fc105639587d.dat
+  ${STEAM_DIR}/${GAME_DIR}/GZW/Content/SKALLA/PrebuildWorldData/World/cache/0xb9af63cee2e43b6c_0x3cb3b3354fb31606.dat
 )
 
 while file in "${FILES[@]}"; do
-	if [ ! -f "${file}" ]
-	then
-		echo "Game file not found. Something is very wrong! (Missing: ${file})" >&2
-		exit 1
-	fi
+  if [ ! -f "${file}" ]
+  then
+    echo "Game file not found. Something is very wrong! (Missing: ${file})" >&2
+    exit 1
+  fi
 done
 
 if ! ${REVERT}
 then
-	if [ -z "${STEAM_USERNAME}" ]
-	then
-		read -p "Steam username: " STEAM_USERNAME
-	fi
-	read -s -p "Steam password: " STEAM_PASSWORD
-	echo
+  if [ -z "${STEAM_USERNAME}" ]
+  then
+    read -p "Steam username: " STEAM_USERNAME
+  fi
+  read -s -p "Steam password: " STEAM_PASSWORD
+  echo
 fi
 
 # Apply the update & fix
@@ -114,15 +114,15 @@ echo "Mode of ${FILES[*]} changed to 0755 (vanilla config)"
 
 if ! ${REVERT}
 then
-	echo "Updating game files..."
-	$STEAMCMD \
-		+force_install_dir "${STEAM_DIR}" \
-		+login "${STEAM_USERNAME} ${STEAM_PASSWORD}" \
-		+app_update 2479810 \
-		+quit
-	echo "Game files updated!"
-	chmod 0444 ${FILES[@]}
-	echo "Mode of ${FILES[*]} changed to 0444 (world read-only)"
+  echo "Updating game files..."
+  $STEAMCMD \
+    +force_install_dir "${STEAM_DIR}" \
+    +login "${STEAM_USERNAME} ${STEAM_PASSWORD}" \
+    +app_update 2479810 \
+    +quit
+  echo "Game files updated!"
+  chmod 0444 ${FILES[@]}
+  echo "Mode of ${FILES[*]} changed to 0444 (world read-only)"
 fi
 
 echo "Done! Happy gaming!"
